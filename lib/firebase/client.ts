974ea2bd98236a0407d931f4ffd4cb86a457090b
Ayushman Bhattacharya <ayushman.rick007@gmail.com>
2026-07-07 14:33:23 +0530

    firebase fixed

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -1,15 +1,14 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsFGlCbz-Hd7Zaw46FVYchEqjLZljwnNk",
-  authDomain: "airesearch-51249.firebaseapp.com",
-  projectId: "airesearch-51249",
-  storageBucket: "airesearch-51249.firebasestorage.app",
-  messagingSenderId: "976236944344",
-  appId: "1:976236944344:web:505d1d3faff111d4d8b0ad",
-  measurementId: "G-EW4DZ2M7E9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase client-side, verifying if already initialized to support HMR
